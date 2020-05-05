517bd168083228fbe3dc54101de02d0f8633924d
Courtney <cjacobs124@gmail.com>
2020-05-05 17:27:10 -0400

    getting there

--- pages/views.py
+++ pages/views.py
@@ -1,18 +1,23 @@
 from django.shortcuts import render, redirect
 from .models import Time
+from .forms import TimeForm
+from django.contrib import messages
 
 # Create your views here.
 
 #HOME
 def home(request):
-	return render(request, "home.html", {})
+	all_appointments = Time.objects.all 
+	return render(request, "home.html", {'all_appointments': all_appointments})
 
 #NEWPAGE
 def appointment(request):
-	return render(request, "newpage.html", {})
+	return render(request, "newpage.html", {}) 
 
 #ABOUT
 def schedules(request):
-	return render(request, "about.html", {})
+	all_appointments = Time.objects.all 
+	#schedules = ['1','2','3','4']
+	return render(request, "about.html", {'all_appointments': all_appointments}) # Passing in all_appointments as all_appointments
 
 			
