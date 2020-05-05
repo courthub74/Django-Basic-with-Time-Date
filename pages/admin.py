811469dda4e8f1067fe6b1837bc911011c276566
Courtney <cjacobs124@gmail.com>
2020-05-05 14:37:47 -0400

    Add Time Table

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Time
 
 # Register your models here.
+# BELOW registers the Time class through admin area
+
+admin.site.register(Time)
+
+
+# after this do:
+	# python manage.py makemigrations
+	# python manage.py migrate
--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+#Create a Class here
+
+class Time(models.Model):
+	date = models.CharField(max_length=200)
+	start = models.CharField(max_length=200)
+	end = models.CharField(max_length=200)
+
+	def __str__(self):
+		return self.date
+
+
+
