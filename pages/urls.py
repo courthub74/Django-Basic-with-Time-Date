7edfc4a94779421702a49953a7011475d8f652d3
Courtney <cjacobs124@gmail.com>
2020-05-06 19:55:17 -0400

    Edit added

--- pages/urls.py
+++ pages/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('appointment/', views.appointment, name='appointment'),
     path('schedules/', views.schedules, name='schedules'),
+    path('edit/<list_id>', views.edit, name='edit' )
     
 ]
--- pages/views.py
+++ pages/views.py
@@ -29,4 +29,24 @@ def schedules(request):
 	#schedules = ['1','2','3','4']
 	return render(request, "about.html", {'all_appointments': all_appointments}) # Passing in all_appointments as all_appointments
 
+
+#EDIT
+def edit(request, list_id):
+	if request.method == 'POST':
+		current_appointment = Time.objects.get(pk=list_id)
+		form = TimeForm(request.POST or None, instance=current_appointment)
+		if form.is_valid():
+			form.save()
+			messages.success(request, ('Appointment Has Been Edited!'))
+			return redirect('schedules')
+		else:
+			messages.success(request, ('Seems Like There Was an Error...'))
+			return render(request, 'edit.html', {})
+	else:
+		get_appointment = Time.objects.get(pk=list_id)
+		return render(request, 'edit.html', {'get_appointment': get_appointment})
+
+
+#DELETE
+
 			
